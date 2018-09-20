10a17e110285ee6523419b2f3ed4e0c09a9d04f9
lPegz <lhp.battousai@gmail.com>
2018-09-20 09:00:33 -0300

    Replace all whitespaces with tabs.

--- password_generator.go
+++ password_generator.go
@@ -1,4 +1,20 @@
 package password_generator
 
+import "github.com/nu7hatch/gouuid"
+
+type password_configuration struct {
+	generationMethod string
+	pattern string
+	strengthFactor int8
+}
+
+func generate_password(password_configuration password_configuration) string {
+	if password_configuration.generationMethod == "uuid" {
+		return uuid(password_configuration.pattern)
+	}
+}
 
 
+func uuid(pattern string) string {
+	return pattern
+}
