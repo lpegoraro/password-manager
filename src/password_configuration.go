ca57a871aa78385c680b9216af785e36fffca015
lpegoraro <luiz.pegoraro@symphony.com>
2019-06-26 11:00:36 -0300

    Enhanced method of calling and getting on error to find file to return default.

--- src/password_configuration.go
+++ src/password_configuration.go
@@ -10,19 +10,14 @@ import (
 var COMMENDATION = "It is highly recommended to change the default by creating a config file, please run password_manager config"
 
 func GetCurrentConfiguration() PasswordConfiguration {
-	fileConfiguration := LoadFromFile()
-	if fileConfiguration.Method == "" {
-		fmt.Println(COMMENDATION)
-		return GetDefaultConfig()
-	}
-	return fileConfiguration
+	return LoadFromFile()
 }
 
 func GetDefaultConfig() PasswordConfiguration {
 	return PasswordConfiguration {
 		Method: "uuid",
 		Seed:   "pwd_manager_test",
-		Factor: 5,
+		Factor: 4,
 	}
 }
 
@@ -34,12 +29,13 @@ func check(e error) {
 
 func LoadFromFile() PasswordConfiguration {
 	configFile, err := ioutil.ReadFile("./config/password_configuration.json")
-	check(err)
+	if (err != nil) {
+		fmt.Println(COMMENDATION)
+		return GetDefaultConfig()
+	}
 	configuration := PasswordConfiguration{}
 	err2 := json.Unmarshal(configFile, &configuration)
 	check(err2)
-	// TODO Remove this log
-	fmt.Println("Configuration loaded: " + configuration.Method + ", " + configuration.Seed)
 	return configuration
 }
 
