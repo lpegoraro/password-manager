e067ce1734a226cdb24672f98cee688671894863
lpegoraro <pegorarohluiz@gmail.com>
2019-07-01 22:54:17 -0300

    No idea how this works

--- storage/non-encrypted-file.go
+++ storage/non-encrypted-file.go
@@ -19,13 +19,16 @@ var STORAGE_FILE = "~/.secure/.passwordmanager.json"
 
 func saveToFile(passwordEntry PasswordEntry) {
 	file, err := ioutil.ReadFile(STORAGE_FILE)
-	savedPasswords := []StorePasswords( {  } )
+	savedPasswords := []StorePasswords{ storedPasswords: passwordEntry }
 	if err != nil {
-
+		panic(err)
 	}
 	err2 := json.Unmarshal(file, &savedPasswords)
-
-	data = []byte(passwordEntry)
+	if err2 != nil {
+		panic(err2)
+	}
+	append(savedPasswords.storedPasswords, passwordEntry)
+	data := []byte()
 	err := ioutil.WriteFile(STORAGE_FILE, data, 0644)
 
 	if err != nil {
