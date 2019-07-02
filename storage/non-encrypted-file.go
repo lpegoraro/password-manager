94d2dfdf7a78abac4ad9e647ab834ecaa8967046
lpegoraro <luiz.pegoraro@symphony.com>
2019-07-02 14:47:52 -0300

    Complete functions to save file and find in file.

--- storage/non-encrypted-file.go
+++ storage/non-encrypted-file.go
@@ -13,7 +13,7 @@ type PasswordEntry struct {
 }
 
 type StorePasswords struct {
-	StoredPasswords []PasswordEntry `json:"storedPasswords"`
+	StoredPasswords map[string]PasswordEntry `json:"storedPasswords"`
 }
 
 var STORAGE_FILE = "~/.secure/.passwordmanager.json"
@@ -22,13 +22,15 @@ func saveToFile(passwordEntry PasswordEntry) {
 	file, err := ioutil.ReadFile(STORAGE_FILE)
 	savedPasswords := StorePasswords{}
 	if err != nil {
-		savedPasswords.StoredPasswords = []PasswordEntry{}
+		savedPasswords.StoredPasswords = make(map[string]PasswordEntry)
 	}
 	err2 := json.Unmarshal(file, &savedPasswords)
 	if err2 != nil {
 		panic(err2)
 	}
-	savedPasswords.StoredPasswords = append(savedPasswords.StoredPasswords, passwordEntry)
+	passwordKey := passwordEntry.Tag + passwordEntry.Username
+	// append the new passwordEntry to savedPasswords
+	savedPasswords.StoredPasswords[passwordKey] = passwordEntry
 	jsonSavedPasswords, err := json.Marshal(savedPasswords)
 	fmt.Println("Saved Passwords file " + string(jsonSavedPasswords))
 	errorOnWriting := ioutil.WriteFile(STORAGE_FILE, jsonSavedPasswords, 0644)
@@ -38,7 +40,24 @@ func saveToFile(passwordEntry PasswordEntry) {
 	}
 }
 
-func findInFile(tag string) string {
-	ioutil.ReadFile(STORAGE_FILE)
+func findInFile(tag string, username string) string {
+	file, err := ioutil.ReadFile(STORAGE_FILE)
+	if err != nil {
+		fmt.Println("404 - Password not found!")
+		return "";
+	}
+	savedPasswords := StorePasswords{}
+	err2 := json.Unmarshal(file, &savedPasswords)
+	if err2 != nil {
+		panic(err2)
+	}
+	for tagKey, passwordValue := range savedPasswords.StoredPasswords {
+		if tag == tagKey {
+			// TODO Remove this log
+			fmt.Println("Password found " + passwordValue.Password)
+			return passwordValue.Password
+		}
+	}
+	fmt.Println("404 - Password not found!")
 	return "";
 }
