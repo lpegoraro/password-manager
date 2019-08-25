8cbbfa02e895a209721622a4f6b004b82afc1974
Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2019-08-25 09:06:45 -0300

    Prepare for integrate

--- password-manager/password_storage.go
+++ password-manager/password_storage.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/lpegoraro/password-manager/storage"
 )
 
@@ -15,16 +16,15 @@ type StorageFactory interface {
 }
 
 func save(description string, username string, configuration PasswordConfiguration, password string) {
-	storage := storageFactory.getStorage(configuration.Storage)
+	storageType := configuration.Storage
 	passwordStore := storage.PasswordEntry{
 		Tag:      description,
 		Username: username,
 		Password: password,
 	}
-	storageType := 
 	switch storageType {
 	case "NOT_ENCRYPTED_FILE":
-		storage.SaveToFile(passwordStore)
+		storage.Save(passwordStore, configuration.Output)
 		break
 	case "OUTPUT":
 		configuration.Output = true
