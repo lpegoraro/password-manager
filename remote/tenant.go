a426390b0f95278a35f91b293aa6dac924769a0c
Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2019-09-18 06:33:56 -0300

    Fix compilation errors and finished methods.

--- remote/tenant.go
+++ remote/tenant.go
@@ -1,35 +1,39 @@
 package remote
 
-
 import (
 	context "context"
 
-	grpc "google.golang.org/grpc"
+	"github.com/lpegoraro/password-manager/encryption"
 )
 
 type DefaultTenantClient struct {
 	TenantKey  string
 	TenantPort string
-	crypto     DefaultCrytoHelper
+	crypto     *encryption.DefaultCrytoHelper
 }
 
-func (dtc *DefaultTenantClient) Connect(ctx context.Context, req *AddTenantReq) (*TenantValue, error) {}
-	var newTenantValue TenantValue
-	newTenantValue = TenantValue{
-		Port:dtc.TenantPort,
-		Fingerprint:"",
-	}
-	return newTenantValue, nil
-}	
+func (dtc *DefaultTenantClient) Connect(ctx context.Context, req *AddTenantReq) (*TenantValue, error) {
+	response := ServeNewTenant(req.TenantName, req.PubKey)
+	return response, nil
+}
 
-func ServeNewTenant(tenantName, publicKey string) {
+func ServeNewTenant(tenantName string, publicKey string) *TenantValue {
 	var newTenant = DefaultTenantClient{
-		TenantKey: tenantName,
+		TenantKey:  tenantName,
 		TenantPort: GetUnusedPort(),
-		crypto: DefaultCrytoHelper{
+		crypto: &encryption.DefaultCrytoHelper{
 			PemCert: publicKey,
 		},
 	}
+	var fingerprint, err = newTenant.crypto.EncodeFingerprint(tenantName)
+	if err != nil {
+		panic("Error generating fingerprint, please check the public key")
+	}
+	return &TenantValue{
+		Port:        newTenant.TenantPort,
+		Fingerprint: fingerprint,
+	}
+
 }
 
 func GetUnusedPort() string {
